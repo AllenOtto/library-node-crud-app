7207df5efc7c8eaeec642af7fd8fdf84b449a44d
AllenOtto <kephamoturi@gmail.com>
2024-12-16 10:57:49 +0300

    Added redirects for page reload after Post requests for create and edit functions

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -22,7 +22,7 @@ const getBook = async (req, res) => {
 const addBook = async (req, res) => {
     try {
         const book = await Book.create(req.body);
-        res.status(200).json({ message: "Success" });
+        res.status(200).redirect("/");
     } catch(err) {
         res.status(500).json({ message: err.message });
     }
@@ -41,7 +41,7 @@ const updateBook = async (req, res) => {
         }
 
         const updatedBook = await Book.findById(id);
-        res.status(200).json(updatedBook);
+        res.status(200).redirect("/");
 
     } catch(err) {
         res.status(500).json({ message: err.message });
